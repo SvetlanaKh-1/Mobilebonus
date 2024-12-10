e63f0b6c1d29e285d25e7810c0023d7cb554db1e
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-10 16:08:49 +0300

    Change code

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
         int income = 1001;
         int oneBonus = 100;
 
-        int bonus = (income > 1000) ? (income / oneBonus) : 0;
+        int bonus = (income >= 1100) ? (income / oneBonus) : 0;
 
         int totalBalance = balance + income + bonus;
 
+        System.out.println("Бонусные рубли: " + bonus);
         System.out.println("Итоговый баланс: " + totalBalance);
     }
 }
