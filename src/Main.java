06bf89d4d1d2a7d458dd1eeb7b58d49406b51932
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-19 11:48:28 +0300

    Code optimization

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int balance = 300;
         int income = 1001;
-        int oneBonus = 100;
-
-        int bonus = (income >= 1100) ? (income / oneBonus) : 0;
-
+        int bonus = (income > 1000) ? (income / 100) : 0;
         int totalBalance = balance + income + bonus;
 
         System.out.println("Бонусные рубли: " + bonus);
